e737680a129d5e5a28e25581a92f474f1db6ad8a
kelibst <kbooster17@gmail.com>
2020-06-30 16:08:43 +0000

    added home page style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,16 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+import 'bootstrap'
+require ("font_awesome5")
+require ("typingScript")
+import './typingScript'
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+

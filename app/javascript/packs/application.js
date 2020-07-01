d3ee4d244b70a43bcaa35a43bdab67799bbe13c0
kelibst <kbooster17@gmail.com>
2020-07-01 14:54:26 +0000

    added all homepage markups

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,9 +10,11 @@ require("channels")
 
 
 import 'bootstrap'
-require ("font_awesome5")
+import "@fortawesome/fontawesome-free/js/all";
+
 require ("typingScript")
 import './typingScript'
+import './navbar'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

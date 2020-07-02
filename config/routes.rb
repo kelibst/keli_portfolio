2e615ac9ebeeb2878aa6b687a9fb6e5230777282
kelibst <kbooster17@gmail.com>
2020-07-02 11:48:04 +0000

    trying to fix assets failed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root "welcome#home"
-  get 'about' => 'welcome#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
